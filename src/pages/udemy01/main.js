3baca3c226603d1299d56cbf84022fe0cccdf606
Yuki Kanayama <kanayama@spicato.com>
2023-05-21 16:38:09 +0900

    once

--- src/pages/udemy01/main.js
+++ src/pages/udemy01/main.js
@@ -1,1 +1,11 @@
-import "./style.css";
+import App3 from './libs/App3';
+
+import './style.css';
+import '/reset.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const app3 = new App3();
+
+	// レンダリング実施
+	app3.render();
+});
